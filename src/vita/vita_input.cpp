9d94775f59bbba705a34fddefa9675b9baf8c4a7
01y <01ycorrespondence@gmail.com>
2020-04-16 10:49:26 -0600

    Fix whitespace in vita_input.cpp

--- src/vita/vita_input.cpp
+++ src/vita/vita_input.cpp
@@ -34,55 +34,55 @@ int lastautopress = 0;
 
 void TranslateControllerEvent(SDL_Event *ev)
 {
-    int upordown = 0;
-    int btn;
-    SDL_Event ev_new;
-    static const struct 
-    {
-        SDL_Keycode sym;
-        SDL_Scancode scan;
-    } v_keymap[] = 
-    {
-        { SDLK_y, SDL_SCANCODE_Y },                 // Triangle
+	int upordown = 0;
+	int btn;
+	SDL_Event ev_new;
+	static const struct
+	{
+		SDL_Keycode sym;
+		SDL_Scancode scan;
+	} v_keymap[] =
+	{
+		{ SDLK_y, SDL_SCANCODE_Y },                 // Triangle
 #ifdef VITATEST
-        { SDLK_BACKSPACE, SDL_SCANCODE_BACKSPACE},
+		{ SDLK_BACKSPACE, SDL_SCANCODE_BACKSPACE},
 #else
-        { SDLK_RALT, SDL_SCANCODE_RALT },             // Circle
+		{ SDLK_RALT, SDL_SCANCODE_RALT },             // Circle
 #endif
-        { SDLK_RETURN, SDL_SCANCODE_RETURN },       // Cross
-        { SDLK_SPACE, SDL_SCANCODE_SPACE },         // Square
-        { SDLK_SPACE, SDL_SCANCODE_SPACE },         // L Trigger
-        { SDLK_y, SDL_SCANCODE_Y },                 // R Trigger
-        { SDLK_DOWN, SDL_SCANCODE_DOWN },           // D-Down
-        { SDLK_LEFT, SDL_SCANCODE_LEFT },           // D-Left
-        { SDLK_UP, SDL_SCANCODE_UP },               // D-Up
-        { SDLK_RIGHT, SDL_SCANCODE_RIGHT },         // D-Right
-        { SDLK_TAB, SDL_SCANCODE_TAB },             // Select
-        { SDLK_ESCAPE, SDL_SCANCODE_ESCAPE },       // Start
-    };
-    
-    memset(&ev_new, 0, sizeof(SDL_Event));
-    btn = ev->jbutton.button;
+		{ SDLK_RETURN, SDL_SCANCODE_RETURN },       // Cross
+		{ SDLK_SPACE, SDL_SCANCODE_SPACE },         // Square
+		{ SDLK_SPACE, SDL_SCANCODE_SPACE },         // L Trigger
+		{ SDLK_y, SDL_SCANCODE_Y },                 // R Trigger
+		{ SDLK_DOWN, SDL_SCANCODE_DOWN },           // D-Down
+		{ SDLK_LEFT, SDL_SCANCODE_LEFT },           // D-Left
+		{ SDLK_UP, SDL_SCANCODE_UP },               // D-Up
+		{ SDLK_RIGHT, SDL_SCANCODE_RIGHT },         // D-Right
+		{ SDLK_TAB, SDL_SCANCODE_TAB },             // Select
+		{ SDLK_ESCAPE, SDL_SCANCODE_ESCAPE },       // Start
+	};
 
-    if (btn < 0 || btn > 11)
-        return;
-    ev_new.key.keysym.sym = v_keymap[btn].sym;
-    ev_new.key.keysym.scancode = v_keymap[btn].scan;
+	memset(&ev_new, 0, sizeof(SDL_Event));
+	btn = ev->jbutton.button;
 
-    if (ev->type == SDL_JOYBUTTONDOWN)
-    {
-        ev_new.type = ev_new.key.type = SDL_KEYDOWN;
+	if (btn < 0 || btn > 11)
+		return;
+	ev_new.key.keysym.sym = v_keymap[btn].sym;
+	ev_new.key.keysym.scancode = v_keymap[btn].scan;
+
+	if (ev->type == SDL_JOYBUTTONDOWN)
+	{
+		ev_new.type = ev_new.key.type = SDL_KEYDOWN;
 		ev_new.key.state = SDL_PRESSED;
-	    upordown = 1;
-    }
-    else if (ev->type == SDL_JOYBUTTONUP)
-    {
-    	ev_new.type = ev_new.key.type = SDL_KEYUP;
+		upordown = 1;
+	}
+	else if (ev->type == SDL_JOYBUTTONUP)
+	{
+		ev_new.type = ev_new.key.type = SDL_KEYUP;
 		ev_new.key.state = SDL_RELEASED;
-	    upordown = 0;
-    }
+		upordown = 0;
+	}
 
-    SDL_PushEvent(&ev_new);
+	SDL_PushEvent(&ev_new);
 #ifndef VITATEST
 	if (btn == 1)
 	{
@@ -102,24 +102,24 @@ void TranslateControllerEvent(SDL_Event *ev)
 
 void TranslateTouchEvent(SDL_Event *ev)
 {
-    int upordown = 0;
+	int upordown = 0;
 
-    float w = 960.0F; //screen width
-    float m = 760.0F; //midpoint between rows
-    float h = 544.0F; //screen height
-    float fingerx = ev->tfinger.x;
-    float fingery = ev->tfinger.y;
-    if (vid_is_ui_stretched) // adjusts for stretched screen, for expressions of the form (c/w)
-    {
-        w *=  0.75F ; // (4/3) / (16/9)
-        fingerx += 0.166667F; // (1/6), compensates for 4:3 mode being centered on the vita's screen, as opposed to left flushed
-    }
-    // front touch
-    if (ev->tfinger.touchId == 0)
-    {
+	float w = 960.0F; //screen width
+	float m = 760.0F; //midpoint between rows
+	float h = 544.0F; //screen height
+	float fingerx = ev->tfinger.x;
+	float fingery = ev->tfinger.y;
+	if (vid_is_ui_stretched) // adjusts for stretched screen, for expressions of the form (c/w)
+	{
+		w *=  0.75F ; // (4/3) / (16/9)
+		fingerx += 0.166667F; // (1/6), compensates for 4:3 mode being centered on the vita's screen, as opposed to left flushed
+	}
+	// front touch
+	if (ev->tfinger.touchId == 0)
+	{
 	if (ev->type == SDL_FINGERDOWN)
 	{
-	    upordown = 1;
+		upordown = 1;
 	}
 	else if (ev->type == SDL_FINGERUP)
 	{
@@ -127,143 +127,143 @@ void TranslateTouchEvent(SDL_Event *ev)
 	}
 
 	if (fingerx > 660.0F / w && fingerx < 860.0F / w)
-        //column containing elevator buttons
-        {
-            if (fingery > 50.0F / h  && fingery <= 139.0F / h)
-            //9,10  50-140 
-            {
-                if (fingerx < m/w )
-                {
-			DepressGivenKey(9, upordown);
-                }
-                else
-                {
-			DepressGivenKey(0, upordown);
-                }
-            }
-            //7,8   140-194
-            if (fingery > 139.0F / h  && fingery <= 194.0F / h)
-            {
-                if (fingerx < m/w )
-                {
-			DepressGivenKey(7, upordown);
-                }
-                else
-                {
-			DepressGivenKey(8, upordown);
-                }
-            }
-            //5,6   194-249
-            if (fingery > 194.0F / h  && fingery <= 249.0F / h)
-            {
-                if (fingerx < m/w )
-                {
-			DepressGivenKey(5, upordown);
-                }
-                else
-                {
-			DepressGivenKey(6, upordown);
-                }
-            }
-            //3,4   249-303
-            if (fingery > 249.0F / h  && fingery <= 304.0F / h)
-            {
-                if (fingerx < m/w )
-                {
-			DepressGivenKey(3, upordown);
-                }
-                else
-                {
-			DepressGivenKey(4, upordown);
-                }
-            }
-            //1,2   303-410
-            if (fingery > 304.0F / h  && fingery <= 410.0F / h)
-            {
-                if (fingerx < m/w )
-                {
-			DepressGivenKey(1, upordown);
-                }
-                else
-                {
-			DepressGivenKey(2, upordown);
-                }
-            }
-        }
-        else
-        //outside of the column
-        {
-        }
-        if (fingery > 410.0F / h)
-        {
-            if (fingerx > 480.0F / w)
-            {
-		        DepressGivenKey(11, upordown);
-            }
-            else
-            {
-		        DepressGivenKey(12, upordown);
-            }
-        }
-        if (fingery < 50.0F / h)
-        {
+		//column containing elevator buttons
+		{
+			if (fingery > 50.0F / h  && fingery <= 139.0F / h)
+			//9,10  50-140
+			{
+				if (fingerx < m/w )
+				{
+					DepressGivenKey(9, upordown);
+				}
+				else
+				{
+					DepressGivenKey(0, upordown);
+				}
+			}
+			//7,8   140-194
+			if (fingery > 139.0F / h  && fingery <= 194.0F / h)
+			{
+				if (fingerx < m/w )
+				{
+					DepressGivenKey(7, upordown);
+				}
+				else
+				{
+					DepressGivenKey(8, upordown);
+				}
+			}
+			//5,6   194-249
+			if (fingery > 194.0F / h  && fingery <= 249.0F / h)
+			{
+				if (fingerx < m/w )
+				{
+					DepressGivenKey(5, upordown);
+				}
+				else
+				{
+					DepressGivenKey(6, upordown);
+				}
+			}
+			//3,4   249-303
+			if (fingery > 249.0F / h  && fingery <= 304.0F / h)
+			{
+				if (fingerx < m/w )
+				{
+					DepressGivenKey(3, upordown);
+				}
+				else
+				{
+					DepressGivenKey(4, upordown);
+				}
+			}
+			//1,2   303-410
+			if (fingery > 304.0F / h  && fingery <= 410.0F / h)
+			{
+				if (fingerx < m/w )
+				{
+					DepressGivenKey(1, upordown);
+				}
+				else
+				{
+					DepressGivenKey(2, upordown);
+				}
+			}
+		}
+		else
+		{
+			//outside of the column
+		}
+		if (fingery > 410.0F / h)
+		{
+			if (fingerx > 480.0F / w)
+			{
+				DepressGivenKey(11, upordown);
+			}
+			else
+			{
+				DepressGivenKey(12, upordown);
+			}
+		}
+		if (fingery < 50.0F / h)
+		{
 
-            if (fingerx > 480.0F / w)
-            {
-		        if ((lastautopress == 0) and (upordown == 1))
-		        {
-			        lastautopress = GetNextWeaponNum();
-			        DepressGivenKey(lastautopress, upordown);
-		        }
-		        else
-		        {
-			        DepressGivenKey(lastautopress, 0);
-			        lastautopress = 0;
-		        }
-            }
-            else //left side
-	        {
-		        if ((lastautopress == 0) and (upordown == 1))
-		        {
-			        lastautopress = GetPrevWeaponNum();
-			        DepressGivenKey(lastautopress, upordown);
-		        }
-		        else
-		        {
-			        DepressGivenKey(lastautopress, 0);
-			        lastautopress = 0;
-		        }
-	        }
-	    }
-    }
-    else
-    {
-        // back touch
-    }
+			if (fingerx > 480.0F / w)
+			{
+				if ((lastautopress == 0) and (upordown == 1))
+				{
+					lastautopress = GetNextWeaponNum();
+					DepressGivenKey(lastautopress, upordown);
+				}
+				else
+				{
+					DepressGivenKey(lastautopress, 0);
+					lastautopress = 0;
+				}
+			}
+			else //left side
+			{
+				if ((lastautopress == 0) and (upordown == 1))
+				{
+					lastautopress = GetPrevWeaponNum();
+					DepressGivenKey(lastautopress, upordown);
+				}
+				else
+				{
+					DepressGivenKey(lastautopress, 0);
+					lastautopress = 0;
+				}
+			}
+		}
+	}
+	else
+	{
+		// back touch
+	}
 
 }
 
 void TranslateAnalogEvent(SDL_Event *ev)
 {
-    static const int deadzone = 512;
-    int delta;
-    delta = ev->jaxis.value;
-    if (delta >= -deadzone && delta <= deadzone)
-    {   
-        delta = 0;
-    }
-    if (ev->jaxis.axis == 0)  //side-to-side
-    {
-        control2x = static_cast<int> (delta * (1.0F + (::mouseadjustment / 6.0F)) / 1777.0F);
-    }
-    else if (ev->jaxis.axis == 2) //turn
-    {
-        in_mouse_dx = delta / 1677 ;
-    }
-    else if (ev->jaxis.axis == 1) //forward
-    {
-        in_mouse_dy = delta / 6655 ;
-    }
+	static const int deadzone = 512;
+	int delta;
+	delta = ev->jaxis.value;
+	if (delta >= -deadzone && delta <= deadzone)
+	{
+		delta = 0;
+	}
+	if (ev->jaxis.axis == 0)  //side-to-side
+	{
+		control2x = static_cast<int> (delta * (1.0F + (::mouseadjustment / 6.0F)) / 1777.0F);
+	}
+	else if (ev->jaxis.axis == 2) //turn
+	{
+		in_mouse_dx = delta / 1677 ;
+	}
+	else if (ev->jaxis.axis == 1) //forward
+	{
+		in_mouse_dy = delta / 6655 ;
+	}
 }
 
 int GetNextWeaponNum()
@@ -272,27 +272,27 @@ int GetNextWeaponNum()
 	std::int8_t uw = gamestate.useable_weapons;
 	if ((cw < wp_pistol) && (uw & (1 << wp_pistol)))
 	{
-        	return 2;
+		return 2;
 	}
 	else if ((cw < wp_burst_rifle) && (uw & (1 << wp_burst_rifle)))
 	{
-        	return 3;
+		return 3;
 	}
 	else if ((cw < wp_ion_cannon) && (uw & (1 << wp_ion_cannon)))
 	{
-        	return 4;
+		return 4;
 	}
 	else if ((cw < wp_grenade) && (uw & (1 << wp_grenade)))
 	{
-        	return 5;
+		return 5;
 	}
 	if ((cw < wp_bfg_cannon) && (uw & (1 << wp_bfg_cannon)))
 	{
-        	return 6;
+		return 6;
 	}
 	else
 	{
-        	return 1;
+		return 1;
 	}
 }
 
@@ -303,27 +303,27 @@ int GetPrevWeaponNum()
 	if (cw == wp_autocharge) cw = wp_SPACER;
 	if ((cw > wp_bfg_cannon) && (uw & (1 << wp_bfg_cannon)))
 	{
-        	return 6;
+		return 6;
 	}
 	else if ((cw > wp_grenade) && (uw & (1 << wp_grenade)))
 	{
-        	return 5;
+		return 5;
 	}
 	else if ((cw > wp_ion_cannon) && (uw & (1 << wp_ion_cannon)))
 	{
-        	return 4;
+		return 4;
 	}
 	else if ((cw > wp_burst_rifle) && (uw & (1 << wp_burst_rifle)))
 	{
-        	return 3;
+		return 3;
 	}
 	else if ((cw > wp_pistol) && (uw & (1 << wp_pistol)))
 	{
-        	return 2;
+		return 2;
 	}
 	else
 	{
-        	return 1;
+		return 1;
 	}
 }
 
@@ -338,80 +338,80 @@ void DepressGivenKey(int whichkey, int upordown)
 		case 1:
 		{
 			ev_new.key.keysym.sym = SDLK_1;
-            ev_new.key.keysym.scancode = SDL_SCANCODE_1;
+			ev_new.key.keysym.scancode = SDL_SCANCODE_1;
 			break;
 		}
 		case 2:
 		{
 			ev_new.key.keysym.sym = SDLK_2;
-            ev_new.key.keysym.scancode = SDL_SCANCODE_2;
+			ev_new.key.keysym.scancode = SDL_SCANCODE_2;
 			break;
 		}
 		case 3:
 		{
 			ev_new.key.keysym.sym = SDLK_3;
-            ev_new.key.keysym.scancode = SDL_SCANCODE_3;
+			ev_new.key.keysym.scancode = SDL_SCANCODE_3;
 			break;
 		}
 		case 4:
 		{
 			ev_new.key.keysym.sym = SDLK_4;
-            ev_new.key.keysym.scancode = SDL_SCANCODE_4;
+			ev_new.key.keysym.scancode = SDL_SCANCODE_4;
 			break;
 		}
 		case 5:
 		{
 			ev_new.key.keysym.sym = SDLK_5;
-            ev_new.key.keysym.scancode = SDL_SCANCODE_5;
+			ev_new.key.keysym.scancode = SDL_SCANCODE_5;
 			break;
 		}
 		case 6:
 		{
 			ev_new.key.keysym.sym = SDLK_6;
-            ev_new.key.keysym.scancode = SDL_SCANCODE_6;
+			ev_new.key.keysym.scancode = SDL_SCANCODE_6;
 			break;
 		}
 		case 7:
 		{
 			ev_new.key.keysym.sym = SDLK_7;
-            ev_new.key.keysym.scancode = SDL_SCANCODE_7;
+			ev_new.key.keysym.scancode = SDL_SCANCODE_7;
 			break;
 		}
 		case 8:
 		{
 			ev_new.key.keysym.sym = SDLK_8;
-            ev_new.key.keysym.scancode = SDL_SCANCODE_8;
+			ev_new.key.keysym.scancode = SDL_SCANCODE_8;
 			break;
 		}
 		case 9:
 		{
 			ev_new.key.keysym.sym = SDLK_9;
-            ev_new.key.keysym.scancode = SDL_SCANCODE_9;
+			ev_new.key.keysym.scancode = SDL_SCANCODE_9;
 			break;
 		}
 		case 0:
 		{
 			ev_new.key.keysym.sym = SDLK_0;
-            ev_new.key.keysym.scancode = SDL_SCANCODE_0;
+			ev_new.key.keysym.scancode = SDL_SCANCODE_0;
 			break;
 		}
 		case 11:
 		{
 			ev_new.key.keysym.sym = SDLK_EQUALS;
-            ev_new.key.keysym.scancode = SDL_SCANCODE_EQUALS;
+			ev_new.key.keysym.scancode = SDL_SCANCODE_EQUALS;
 			break;
 		}
 		case 12:
 		{
 			ev_new.key.keysym.sym = SDLK_MINUS;
-            ev_new.key.keysym.scancode = SDL_SCANCODE_MINUS;
+			ev_new.key.keysym.scancode = SDL_SCANCODE_MINUS;
 			break;
 		}
 		default:
-        {
-            ev_new.key.keysym.sym = SDLK_0;
-            ev_new.key.keysym.scancode = SDL_SCANCODE_0;
-        }
+		{
+			ev_new.key.keysym.sym = SDLK_0;
+			ev_new.key.keysym.scancode = SDL_SCANCODE_0;
+		}
 	}
 	if (upordown != 0)
 	{
@@ -426,4 +426,3 @@ void DepressGivenKey(int whichkey, int upordown)
 
 	SDL_PushEvent(&ev_new);
 }
-
